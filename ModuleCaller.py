83c8e8a342fb2b80689b1724de22e1d79fd8df5c
HarishKannanE <harishkannan944@gmail.com>
2023-06-23 15:48:21 +0530

    commit 2

---
+++ ModuleCaller.py
@@ -0,0 +1,12 @@
+import Module
+
+from Module import *
+
+# print(dir(Module))
+print("")
+Module.order_food("Salad", "Biriyani")
+print("")
+Module.vac_feedback("Moderna", 30)
+print("")
+Module.time_activity(20, hobby="Dance")
+print("")
---
+++ ModuleCaller2.py
@@ -0,0 +1,7 @@
+from Module import *
+
+order_food("Pizza")
+
+vac_feedback("Moderna", 55)
+
+time_activity(30, Job="DevOps")
